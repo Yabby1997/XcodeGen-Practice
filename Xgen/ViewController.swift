fe7cbd093576a7bce0f6525b2d771d067bf267a7
Yabby1997 <yabby1997@gmail.com>
2022-10-14 11:47:18 +0900

    Initial Commit

---
+++ Xgen/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Xgen
+//
+//  Created by USER on 2022/10/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("HELLO")
+    }
+}
+
